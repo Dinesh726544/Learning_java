d04cf03455de52f4f162502d0881f9fc7c993b26
Dinesh726544 <bobi726544@gmail.com>
2024-03-17 22:59:28 +0530

    done with basics

---
+++ day01/Basics.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Basics {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);        //use to take input
+        System.out.print("Enter your name:");
+        String name = sc.nextLine();                //we have taken input as string
+        System.out.println(name);
+    }
+}
